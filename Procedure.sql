340689f97c4420d13a33780622b3f38d6c050fc4
Dipikadahifale <131863577+Dipikadahifale@users.noreply.github.com>
2023-05-12 16:58:14 +0530

    add files

---
+++ Procedure.sql
@@ -0,0 +1,35 @@
+-- creating the simple PROCEDURE
+DELIMITER //
+create Procedure Reader_number()
+BEGIN
+select Rid ,Rname
+from reader ;
+end //
+DELIMITER ;
+
+-- call the above PROCEDURE
+CALL Reader_number();
+
+-- creating the Parameterized Procedure
+
+DELIMITER //
+create Procedure reader_age(name varchar(30))
+BEGIN
+select * from reader where Rname = name;
+End //
+DELIMITER ;
+
+-- Multiple Parameterized Procedures
+delimiter //
+create PROCEDURE book_info(b_name varchar(30),price int)
+BEGIN
+select * from Books where bname = b_name AND price =Price;
+end //
+delimiter ;
+
+call book_info('wings of fire',200);
+
+-- to drop the PROCEDURE
+DROP PROCEDURE book_info;
+
+-- Author - DIPIKA DHIFALE
---
+++ tigger.sql
@@ -0,0 +1,30 @@
+
+
+-- TRIGGER for the insert  the database 
+Delimiter $$
+create trigger ins_reader
+Before Insert ON
+Reader for each row
+Begin
+Insert into back_reader (Rid ,Rname, Rage,Rmobile,section )
+values (new.Rid  ,new.Rname    , new.Rage ,new.Rmobile   ,new.section  );
+end 
+Delimiter $$
+
+-- TRIGGER for the delete the database from reader table 
+
+Delimiter $$
+create trigger delete_reader
+Before DELETE ON
+Reader for each row
+Begin
+Insert into back_reader (Rid ,Rname, Rage,Rmobile,section )
+values (old.Rid  ,old.Rname    , old.Rage ,old.Rmobile   ,old.section  );
+end 
+Delimiter $$
+
+DELETE  from reader WHERE Rid = 2;
+
+select *from back_reader;
+
+-- Author - DIPIKA DHIFALE
